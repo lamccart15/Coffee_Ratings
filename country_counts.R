7b2e6f9905f30271c2b8b07962b6b386d3955baa
moriahtaylor1 <moriahataylor@gmail.com>
2021-08-10 09:30:10 -0400

    create processing methods df

--- country_counts.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-coffee <- read.csv("https://raw.githubusercontent.com/lamccart15/Coffee_Ratings/main/data/coffee_data_cleaned.csv")
-
-country_counts <- coffee %>% count(country_of_origin)
-
-country_counts[32, 1] <- "USA"
-
-write.csv(country_counts, "data/country_coffee_counts.csv", row.names=FALSE)
