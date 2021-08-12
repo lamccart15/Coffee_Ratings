cc65ed23a0118ef53a87b129fb1ffd6b201a41ac
nmbrouss <nicolebroussard@Nicoles-MacBook-Air.local>
2021-08-11 19:30:36 -0500

    fixed x and y

--- static/js/feat_doughnut.js
+++ static/js/feat_doughnut.js
@@ -2,8 +2,8 @@ var per = [49.9, 17.4, 14.3, 4.56, 4.39, 3.18, 1.79, 1.64, 1.47, 0.778, 0.382, 0
 var imp_fea = ["flavor", "balance", "aftertaste", "body", "aroma", "acidity", "category_two_defects", "category_one_defects", "moisture, uniformity", "sweetness, clean_cup"]
 
 var data = [{
-    x: per,
-    y: imp_fea,
+    values: per,
+    labels: imp_fea,
     type: "pie",
     hole:.5
 }];
