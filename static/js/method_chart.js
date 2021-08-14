107ee41fc959d1617f1cdf8fed5d1003fae5e93c
julichalk <77757079+julichalk@users.noreply.github.com>
2021-08-14 10:32:58 -0400

    Add files via upload

---
+++ static/js/method_chart.js
@@ -0,0 +1,37 @@
+
+
+
+function init(){
+    d3.csv('/api/methods').then(data => {
+    console.log(data)
+        var method = data.map(d => d.processing_method)
+        console.log(method)
+        var count = data.map(d => d.n)
+        console.log(count)
+        
+
+
+    var trace1 = {
+        x: method,
+        y: count,
+        marker:{
+            color: ['#4B2B0F', '#090B06', '#762807', '#EBA937', '#344718','#895324']
+          },
+           type: "bar"
+      };
+     
+      var data = [trace1];
+     
+      var layout = {
+       title: "Processing Methods of Coffee Beans",
+        xaxis: { title: "Processing Method"},
+       yaxis: { title: "Count of Beans Per Processing Method"}
+      };
+     
+Plotly.newPlot("plot", data, layout);
+
+    }); 
+}
+
+
+ init()
